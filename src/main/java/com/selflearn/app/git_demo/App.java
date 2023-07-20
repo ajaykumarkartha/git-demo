a994f790fff0cf0732da7b1505b9630691f47adf
ajaykumarkartha <mail2ajaykartha@gmail.com>
2023-07-20 12:51:44 -0400

    removed comments

--- src/main/java/com/selflearn/app/git_demo/App.java
+++ src/main/java/com/selflearn/app/git_demo/App.java
@@ -1,13 +1,8 @@
 package com.selflearn.app.git_demo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public void addData(String data) {
-		System.out.println("data = " + data);
 		if (data.equals(Constant.DATA_TYPE)) {
 			System.out.println("Welcome");
 		}
